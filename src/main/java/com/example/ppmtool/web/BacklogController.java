dd6cc772486b38798d1979262b922e81eac3a5dd
unknown <anukulpk2@gmail.com>
2023-02-21 11:44:35 +0530

    update project task by project sequence

--- src/main/java/com/example/ppmtool/web/BacklogController.java
+++ src/main/java/com/example/ppmtool/web/BacklogController.java
@@ -25,26 +25,39 @@ public class BacklogController {
     private MapValidationErrorService mapValidationErrorService;
 
     @PostMapping("/{backlog_id}")
-    public ResponseEntity<?> addProjectTaskToBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id){
-        ResponseEntity<?> errorMap  = mapValidationErrorService.MapValidationService(result);
+    public ResponseEntity<?> addProjectTaskToBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
 
-        if(errorMap!=null){
+        if (errorMap != null) {
             return errorMap;
         }
 
-        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id,projectTask);
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
         return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
     }
 
     @GetMapping("/{backlog_id}")
-    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id){
+    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id) {
         return projectTaskService.findBacklogById(backlog_id);
     }
 
     @GetMapping("/{backlog_id}/{pt_id}")
-    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
-        ProjectTask projectTask=projectTaskService.findPTByProjectSequence(backlog_id,pt_id);
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id) {
+        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id, pt_id);
         return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> updateProjectTask(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id, @PathVariable String pt_id) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+
+        if (errorMap != null) {
+            return errorMap;
+        }
+
+        ProjectTask updatedTask = projectTaskService.updateByProjectSequence(projectTask, backlog_id, pt_id);
+
+        return new ResponseEntity<ProjectTask>(updatedTask, HttpStatus.OK);
     }
 
 
